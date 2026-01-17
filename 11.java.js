1b507af465b529a4fbf999b2fae4404b70ffcd47
sinesin23-bit <sinesin23@gmail.com>
2026-01-17 15:20:11 +0300

    Add files via upload

---
+++ 11.java.js
@@ -0,0 +1,16 @@
+function login(){
+    const status= 
+    document.gertElementById("status");
+    status.innerTEXT = "online";
+    status.style.color = "green";
+}
+function logout(){
+    const status = 
+    document.getElementeryById("status");
+    status.innerText = "offline";
+    status. style.color = "red";
+}
+function changecourse(){
+    doucment.getElementById("course").innerText = "computerscience"
+}
+
